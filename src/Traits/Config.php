9b19f77ef34b615a3c36cd9606e18ee6161f4c05
Robert Butler <me@r-butler.net>
2022-07-19 20:51:24 -0400

    Refactoring AppWrite trait

--- src/Traits/Config.php
+++ src/Traits/Config.php
@@ -2,19 +2,33 @@
 use Illuminate\Support\Str,
 	Illuminate\Support\Arr;
 
-use Lighth7015\AppWrite\Helpers\AppWrite;
-
 trait Config {
-	protected function filename(string $file): string {
-		return AppWrite::filename($file);
+		protected function filename(string $file): string {
+		return realpath(Str::finish(__DIR__, Str::finish('/../../', $file)));
 	}
 
-	protected function getProjectName(): string {
-		return AppWrite::project();
+	protected function project(): string {
+		$namespaces = array_map( fn(string $it) => strtolower($it), explode( '\\', __NAMESPACE__ ));
+		$getParentNamespace = fn(int $index) => Arr::get( $namespaces, $index );
+		
+		return Arr::get( config( call_user_func( $getParentNamespace, 1 ), null), "project" );
 	}
 
 	protected function config(string | null ...$keys) {
-		return forward_static_call_array( array( AppWrite::class, 'config' ), $keys );
+		$namespaces = array_map( fn(string $it) => strtolower($it), explode( '\\', __NAMESPACE__ ));
+		$getParentNamespace = fn(int $index) => Arr::get( $namespaces, $index );
+		
+		$path = implode( ".", array_reduce( $keys, function (array $keys, string | null $key) {
+			if (is_string($key)) array_push( $keys, $key );
+			return $keys;
+		}, array()));
+
+		if (is_null( $config = config( call_user_func( $getParentNamespace, 1 ), null)) === false) {
+			$config = Arr::get( $config, Str::finish( "projects.", $this->getProjectName()));
+			return strlen($path) > 0? Arr::get( $config, $path ): $config;
+		}
+		
+		return $config;
 	}
 
 	protected function resolveCredentials(string $param): string {
